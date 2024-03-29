7a2dc01cdb371446b510b276761d4d37af8ce930
caocao0525 <seohyun.lee.physics@gmail.com>
2024-03-29 17:30:51 +0900

    Initial commit

---
+++ examples/crm/script_pre/run_4mer_lim9_all_crm.sh
@@ -0,0 +1,42 @@
+export KMER=4
+
+#lim_len="lim10"
+export TRAIN_FILE=../pretrain_data/lim9/crm_lim9_allcell_4merized.txt
+export TEST_FILE=../pretrain_data/lim9/crm_lim9_allcell_4merized.txt
+
+export SOURCE=../../..
+export OUTPUT_PATH=../pretrain_result/4mer/lim9
+
+export TRAIN_BATCH=5
+export EVAL_BATCH=3
+
+
+python ../../run_pretrain.py \
+ --output_dir $OUTPUT_PATH \
+ --model_type=dna \
+ --tokenizer_name=dna$KMER \
+ --config_name=$SOURCE/src/transformers/dnabert-config/bert-config-$KMER/config.json \
+ --do_train \
+ --train_data_file=$TRAIN_FILE \
+ --do_eval \
+ --eval_data_file=$TEST_FILE \
+ --mlm \
+ --gradient_accumulation_steps 25 \
+ --per_gpu_train_batch_size $TRAIN_BATCH \
+ --per_gpu_eval_batch_size $EVAL_BATCH \
+ --save_steps 500 \
+ --save_total_limit 20 \
+ --max_steps 10000 \
+ --evaluate_during_training \
+ --logging_steps 500 \
+ --line_by_line \
+ --learning_rate 4e-4 \
+ --block_size 512  \
+ --adam_epsilon 1e-6 \
+ --weight_decay 0.01 \
+ --beta1 0.9 \
+ --beta2 0.98 \
+ --mlm_probability 0.025 \
+ --warmup_steps 500  \
+ --overwrite_output_dir \
+ --n_process 24
