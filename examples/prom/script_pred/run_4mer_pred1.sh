84cecaea3bcbd069b1968f99965a32ff8e66bc91
caocao0525 <seohyun.lee.physics@gmail.com>
2024-04-07 10:11:34 +0900

    update

---
+++ examples/prom/script_pred/run_4mer_pred1.sh
@@ -0,0 +1,17 @@
+export KMER=4
+export MODEL_PATH=../ft_result
+export DATA_PATH=../ft_data/all
+export PREDICTION_PATH=../predict_result
+
+    python ../../run_finetune.py \
+        --model_type dna \
+        --tokenizer_name=dna$KMER \
+        --model_name_or_path $MODEL_PATH \
+        --task_name dnaprom \
+        --do_predict \
+        --data_dir $DATA_PATH \
+        --max_seq_length 300 \
+        --per_gpu_pred_batch_size=128   \
+        --output_dir $MODEL_PATH \
+        --predict_dir $PREDICTION_PATH \
+        --n_process 48
---
+++ examples/prom/script_pred/run_4mer_pred2.sh
@@ -0,0 +1,19 @@
+export KMER=4
+export MODEL_PATH=../ft_result
+export DATA_PATH=../ft_data/all
+export PREDICTION_PATH=../predict_result
+
+    python ../../run_finetune.py \
+        --model_type dna \
+        --tokenizer_name=dna$KMER \
+        --model_name_or_path $MODEL_PATH \
+        --task_name dnaprom \
+        --do_visualize \
+        --visualize_data_dir $DATA_PATH \
+        --visualize_models $KMER \
+        --data_dir $DATA_PATH \
+        --max_seq_length 81 \
+        --per_gpu_pred_batch_size=16   \
+        --output_dir $MODEL_PATH \
+        --predict_dir $PREDICTION_PATH \
+        --n_process 96 
